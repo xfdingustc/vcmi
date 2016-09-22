63fd36a6053d120b586d505c33bc52972ca8750c
Arseniy Shestakov <me@arseniyshestakov.com>
2016-09-22 07:56:52 +0300

    Sight map: new version of FoW. Using TeamState fogOfWarMap as storage
    Before FoW code was really messy and slow in cases when tiles are hidden.
    Since we didn't knew what we shouldn't hide we had to re-check every object.
    
    Instead we'll store how many team-owned objects have sight over every tile on map.
    Now we have to be more careful when GS changes, but overall FoW code become much cleaner.

--- lib/CGameState.h
+++ lib/CGameState.h
@@ -227,6 +227,10 @@ public:
 	std::vector<CGObjectInstance*> guardingCreatures (int3 pos) const;
 	void updateRumor();
 
+	void initSightMap();
+	void addSightObj(const CGObjectInstance * obj, bool add = true);
+	void removeSightnObj(const CGObjectInstance * obj);
+
 	// ----- victory, loss condition checks -----
 
 	EVictoryLossCheckResult checkForVictoryAndLoss(PlayerColor player) const;
@@ -307,6 +311,7 @@ private:
 	void initStartingResources();
 	void initHeroes();
 	void giveCampaignBonusToHero(CGHeroInstance * hero);
+	void addSightObj(TeamID team, const CGObjectInstance * obj, bool add = true);
 	void initFogOfWar();
 	void initStartingBonus();
 	void initTowns();
