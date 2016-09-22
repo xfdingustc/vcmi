a63b7b8b96410d65977baa273392706505f64b43
Arseniy Shestakov <me@arseniyshestakov.com>
2016-09-22 07:58:01 +0300

    Sight map: add asserts to make sure we'll notice if something broke
    If something goes wrong FoW that mean it's messed up completely so this code should not break.

--- lib/CGameState.cpp
+++ lib/CGameState.cpp
@@ -1616,6 +1616,7 @@ void CGameState::removeSightnObj(const CGObjectInstance * obj)
 
 void CGameState::addSightObj(const CGObjectInstance * obj, bool add)
 {
+	assert(obj);
 	if(!vstd::contains(players, obj->tempOwner))
 		return;
 
@@ -1633,6 +1634,9 @@ void CGameState::addSightObj(TeamID team, const CGObjectInstance * obj, bool add
 	getTilesInRange(tiles, obj->getSightCenter(), obj->getSightRadius());
 	for(int3 t : tiles)
 	{
+		/// This code expect that tile can't be within sight range of more than 254 player-owned objects
+		/// Assert is best way to do these checks since if they fail that's mean code is broken
+		assert(ts->fogOfWarMap[t.x][t.y][t.z] != 255);
 		if(add)
 		{
 			if(ts->fogOfWarMap[t.x][t.y][t.z] == FoWChange::HIDDEN)
@@ -1642,6 +1646,9 @@ void CGameState::addSightObj(TeamID team, const CGObjectInstance * obj, bool add
 		}
 		else
 		{
+			/// There was no objects with sight over this tile, but for some reason we try to remove sight anyway
+			/// That's mean when ownership or sight radius of object changed sight map wasn't appropriately updated
+			assert(ts->fogOfWarMap[t.x][t.y][t.z] == FoWChange::REVEALED);
 			ts->fogOfWarMap[t.x][t.y][t.z]--;
 		}
 	}
